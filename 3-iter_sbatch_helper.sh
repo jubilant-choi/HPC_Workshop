7d1067c50225c1bf96575f0351aabe3603c52d62
jubilant-choi <wnqlszoq123@snu.ac.kr>
2025-04-02 04:30:01 +0900

    change filename

---
+++ 3-iter_sbatch_helper.sh
@@ -0,0 +1,17 @@
+sbatch_script="3-systematic_sbatch.sh"
+
+curr_date=$(date +'%y%m%d-%H%M')
+job_name="${curr_date}_systematic_lr_wd_maskratio"
+
+lrs=(1e-1 1e-2)
+weight_decays=(1e-1 1e-2)
+mask_ratios=(0.4 0.8)
+
+for lr in "${lrs[@]}"; do
+    for wd in "${weight_decays[@]}"; do
+        for mask_ratio in "${mask_ratios[@]}"; do
+            echo sbatch --job-name="${job_name}" "${sbatch_script}" $lr $wd $mask_ratio
+            sbatch --job-name="${job_name}" "${sbatch_script}" $lr $wd $mask_ratio
+        done
+    done
+done
